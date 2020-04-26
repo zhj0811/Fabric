d6bbfabd842aea1885313641f7a083f49a31dc85
zhj0811 <1530005648@qq.com>
2020-04-26 10:58:31 +0800

    feat: add chaincode_test.go file

--- define/factor.go
+++ define/factor.go
@@ -3,8 +3,8 @@ package define
 //go:generate easyjson -all factor.go
 // Factory struct
 type Factory struct {
-	Key     string `json:"key"`     //业务key
-	Value   string `json:"value"`   // 业务数据
-	Expand1 string `json:"expand1"` // 扩展字段1
-	Expand2 string `json:"expand2"` // 扩展地段2
+	Key     string `json:"key"`               //业务key
+	Value   string `json:"value"`             // 业务数据
+	Expand1 string `json:"expand1,omitempty"` // 扩展字段1
+	Expand2 string `json:"expand2,omitempty"` // 扩展地段2
 }
